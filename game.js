7066e8c24282202745af4accbbd1b248597bbfac
Robert Hill <codingbyrobert@gmail.com>
2015-10-19 12:54:47 -0700

    cleaned up js a little

--- game.js
+++ game.js
@@ -1,8 +1,18 @@
-var chooseCharButton = document.getElementById('chooseChar');
 
+//unhides the char creation form-REH
 var startCharCreation = function(e) {
   e.preventDefault();
-  var form = document.getElementById('charCreationForm');
-   form.style.visibility = 'visible'
+  document.getElementById('charCreationForm').style.visibility = 'visible';
+
 }
-chooseCharButton.addEventListener('click', startCharCreation);
+//Choose char button
+document.getElementById('chooseChar').addEventListener('click', startCharCreation);
+
+
+//processes char Creation form data and saves to local storage-REH
+var startGame = function(e) {
+  e.preventDefault();
+  var q1 = document.getElementsByName('q1');
+}
+document.getElementById('startGame').addEventListener('click', startGame);
+
