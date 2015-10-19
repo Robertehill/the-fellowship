505465b91984798a76b57c51fee22cceb29dbf11
Gina Lee Gossett <ginaleegossett@Ginas-MacBook-Pro.local>
2015-10-19 14:46:34 -0700

    added JS to tally radio votes

--- README.md
+++ README.md
@@ -4,3 +4,7 @@ Monday, October 19th
 Morning: 	Gina and Robin created style.css and updated index.html and game.js
 			Robert worked the character form
 			Pat worked on header
+
+Afternoon: 	Pat and Robin work on CSS, styling, index.html
+			Robert and Gina work on startGame function
+				For startGame function, we put the form information in a for loop to tally up the checked radio buttons
--- game.js
+++ game.js
@@ -6,11 +6,61 @@ var startCharCreation = function(e) {
 //Choose char button-REH
 document.getElementById('chooseChar').addEventListener('click', startCharCreation);
 
-//processes char Creation form data and saves to local storage-REH
+var elf = 0;
+var dwarf = 0;
+var hobbit = 0;
+
+//processes char Creation form data and saves to local storage-REH/GLG
 var startGame = function(e) {
   e.preventDefault();
-  var q1 = document.getElementsByName('q1');
+
+	var q1 = document.getElementsByName('q1');
+	for (var i=0; i < q1.length; i++) {
+		if (q1[i].checked) {
+			if (q1[i].value === "a1") {
+				elf += 1;
+			}
+			else if (q1[i].value === "a2") {
+				dwarf += 1;
+			}
+			else if (q1[i].value === "a3") {
+				hobbit += 1;
+			}
+		}
+	}
+
+	var q2 = document.getElementsByName('q2');
+	for (var i=0; i < q2.length; i++) {
+		if (q2[i].checked) {
+			if (q2[i].value === "a1") {
+				elf += 1;
+			}
+			else if (q2[i].value === "a2") {
+				dwarf += 1;
+			}
+			else if (q2[i].value === "a3") {
+				hobbit += 1;
+			}
+		}
+	}	
+
+	var q3 = document.getElementsByName('q3');
+	for (var i=0; i < q3.length; i++) {
+		if (q3[i].checked) {
+			if (q3[i].value === "a1") {
+				elf += 1;
+			}
+			else if (q3[i].value === "a2") {
+				dwarf += 1;
+			}
+			else if (q3[i].value === "a3") {
+				hobbit += 1;
+			}
+		}
+	}
 }
+
+
 //start game Button-REH
 document.getElementById('startGame').addEventListener('click', startGame);
 
