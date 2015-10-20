bc1fbabfde2aa279aad8c3c77315bb40685a22cd
Pat Yau <patrickyau@Pat-Yaus-MacBook-Pro.local>
2015-10-20 14:02:57 -0700

    added minor error correction line

--- game1.js
+++ game1.js
@@ -22,9 +22,9 @@ function Event(img, head, para, button1, button2, buttonId, deathImg, deathHead,
 	this.button1 = button1;
 	this.button2 = button2;
 	this.buttonId = buttonId; 
-	this.deathImg = deathImg;
-	this.deathHead = deathHead;
-	this.deathPara = deathPara;
+	// this.deathImg = deathImg;
+	// this.deathHead = deathHead;
+	// this.deathPara = deathPara;
 
 	this.loadEvent = function(loc) {
 		// var butt1 = document.createElement('button')		
@@ -32,11 +32,21 @@ function Event(img, head, para, button1, button2, buttonId, deathImg, deathHead,
 	
 		loc.innerHTML = '<img src="' + img + '" /><h2>' + head + '</h2><p>' + para + '</p> <button id="' + buttonId + 'button1" type="click">' + button1 + '</button><button id="' + buttonId + 'button2" type="click">' + button2 + '</button>';
 
-		var y = this.buttonId + 'button1';
-		document.getElementById(y).addEventListener('click', function(){
+		var option1 = this.buttonId + 'button1';
+		document.getElementById(option1).addEventListener('click', function(){
 			// var x = document.getElementById(loc).nextSibling;
 			// deathEvent(event, get_nextsibling(loc), this.deathPara);
 			get_nextsibling(loc).innerHTML = '<img src="' + deathImg + '" /><h2>' + deathHead + '</h2><p>' + deathPara + '</p>';
+		});
+
+		var option2 = this.buttonId + 'button2';
+		document.getElementById(option2).addEventListener('click', function(){
+			// var x = document.getElementById(loc).nextSibling;
+			// deathEvent(event, get_nextsibling(loc), this.deathPara);
+			// get_nextsibling(loc).innerHTML = '<img src="' + event2Dwarf.img + '" /><h2>' + event2Dwarf.head + '</h2><p>' + event2Dwarf.para + '</p>';
+			if (option2 === 'startbutton2'){
+				event2Dwarf.loadEvent(event2);
+			} 	
 		});
 
 		// 	get_nextsibling(loc).innerHTML = deathPara;
@@ -51,15 +61,13 @@ var deathEvent = function(e, loc, deathPara) {
 	e.preventDefault();
 }
 
-var startDwarfDeath = new Event('img/game/death.jpg', 'death title', 'death text here. you\'re dead.', 'go back', 'end game', 'death button');
-
-var startDwarf = new Event('img/game/start.jpg', 'Congrats username are a dwarf!', 'Lorem ipsum dolor sit amet, sonet elitr usu cu. Voluptaria mediocritatem nam at, nam probo dolorum debitis ex. Ne vide aeque duo, qui enim invenire mnesarchum et.', 'startDwarf Button1', 'startDwarf Button2', 'start', 'img/game/death.jpg', 'startDwarf death headline', 'death text here. you\'re dead.');
+var event1Dwarf = new Event('img/game/event1Dwarf.jpg', 'Congrats username are a dwarf!', 'Lorem ipsum dolor sit amet, sonet elitr usu cu. Voluptaria mediocritatem nam at, nam probo dolorum debitis ex. Ne vide aeque duo, qui enim invenire mnesarchum et.', 'event1Dwarf Button1', 'event1Dwarf Button2', 'start', 'img/game/death.jpg', 'event1Dwarf death headline', 'death text here. you\'re dead.');
 // var startElf = new Event('intro', 'img/game/start.jpg', 'Congrats username are a elf!', 'Lorem ipsum dolor sit amet, sonet elitr usu cu. Voluptaria mediocritatem nam at, nam probo dolorum debitis ex. Ne vide aeque duo, qui enim invenire mnesarchum et.', 'startElf Button 1', 'startElf Button2');
 // var startHobbit = new Event('intro', 'img/game/start.jpg', 'Congrats username are a hobbit!', 'Lorem ipsum dolor sit amet, sonet elitr usu cu. Voluptaria mediocritatem nam at, nam probo dolorum debitis ex. Ne vide aeque duo, qui enim invenire mnesarchum et.', 'startHobbit Button 1', 'startHobbit Button2');
 
-var event1Dwarf = new Event('img/game/event1.jpg', 'Event 1 headline!', 'Lorem ipsum dolor sit amet, sonet elitr usu cu. Voluptaria mediocritatem nam at, nam probo dolorum debitis ex. Ne vide aeque duo, qui enim invenire mnesarchum et.', 'event1Dwarf Button 1', 'eventDwarf Button2', 'event1Dwarf');
+var event2Dwarf = new Event('img/game/event2.jpg', 'Event 2 headline!', 'Lorem ipsum dolor sit amet, sonet elitr usu cu. Voluptaria mediocritatem nam at, nam probo dolorum debitis ex. Ne vide aeque duo, qui enim invenire mnesarchum et.', 'event2Dwarf Button 1', 'eventDwarf Button2', 'event2Dwarf');
 
-startDwarf.loadEvent(event1);
+event1Dwarf.loadEvent(event1);
 // event1Dwarf.loadEvent(event1);
 
 
